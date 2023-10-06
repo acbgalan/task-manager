fd2bd75701b9ed40ce86c57d72900469dff38bfd
Carlos Blanco <acbgalan@gmail.com>
2023-10-07 01:04:47 +0200

    Ajustando módelos para la bd

--- src/task-manager.data/Models/Task.cs
+++ src/task-manager.data/Models/Task.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,18 @@ namespace task_manager.data.Models
 {
     public class Task
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+
+        [DisplayName("Tarea")]
+        public string Name { get; set; }
+
+        [DisplayName("Aviso")]
+        public DateTime? RemenberMe { get; set; }
+
+        [DisplayName("Fecha de vencimiento")]
+        public DateTime? ExpirationTime { get; set; }
+
     }
 }
