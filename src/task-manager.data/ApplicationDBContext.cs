97778c9259de23eeb47e293c3075c99066ed6d29
carlosblanco.dev <acbgalan@gmail.com>
2023-10-09 12:54:35 +0200

    Contexto par ala base de datos y limpieza clase

---
+++ src/task-manager.data/ApplicationDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using task_manager.data.Models;
+
+namespace task_manager.data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Task> Tasks { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
--- src/task-manager.data/Models/Task.cs
+++ src/task-manager.data/Models/Task.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace task_manager.data.Models
 {
@@ -27,6 +22,5 @@ namespace task_manager.data.Models
         public DateTime? ExpirationTime { get; set; }
 
         public List<Category> Categories { get; set; }
-
     }
 }
