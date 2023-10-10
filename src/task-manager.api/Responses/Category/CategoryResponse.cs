bad5797bc7a8eabd3e487d0727f11331c6db2a5c
carlosblanco.dev <acbgalan@gmail.com>
2023-10-11 00:54:17 +0200

    CategoryResponse

---
+++ src/task-manager.api/Responses/Category/CategoryResponse.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace task_manager.api.Responses.Category
+{
+    public class CategoryResponse
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Categoría")]
+        public string Name { get; set; }
+    }
+}
