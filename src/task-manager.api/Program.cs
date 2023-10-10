ac8f45965a842cdf8656f6d934255c68bffd2174
Carlos Blanco <acbgalan@gmail.com>
2023-10-10 14:05:57 +0200

    Configura inyección de dependencia para el repositorio de Category

--- src/task-manager.api/Program.cs
+++ src/task-manager.api/Program.cs
@@ -1,10 +1,13 @@
 using Microsoft.EntityFrameworkCore;
 using task_manager.data;
+using task_manager.data.Repositories;
+using task_manager.data.Repositories.Interface;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<ApplicationDBContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ApplicationDefault")));
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
