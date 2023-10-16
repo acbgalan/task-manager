db63f31735944db4ef60e9ab11eb5a811c14383e
Carlos Blanco <acbgalan@gmail.com>
2023-10-16 15:45:44 +0200

    Migración EF. Relación entre Task y Step

---
+++ src/task-manager.data/Migrations/20231016134448_Task_Has_Steps.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace task_manager.data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Task_Has_Steps : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Tasks",
+                newName: "Description");
+
+            migrationBuilder.CreateTable(
+                name: "Steps",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Description = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    TaskId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Steps", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Steps_Tasks_TaskId",
+                        column: x => x.TaskId,
+                        principalTable: "Tasks",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Steps_TaskId",
+                table: "Steps",
+                column: "TaskId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Steps");
+
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "Tasks",
+                newName: "Name");
+        }
+    }
+}
